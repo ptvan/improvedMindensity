f1eaec89d1cafaac1e1954c6ee9df6533f855ae3
ptvan <pvan@fhcrc.org>
2015-09-18 09:36:19 -0700

    added example workflow code

---
+++ workflow.R
@@ -0,0 +1,83 @@
+# example workflow of using improvedMindensity() and associated functions
+# to improve 1D gating of cyTOF samples
+# detailed usage are in comments of improvedMindensity.R
+
+library(openCyto)
+library(data.table)
+library(latticeExtra)
+library(plyr)
+library(ggplot2)
+
+dataPath <- "/shared/silo_researcher/Gottardo_R/pvan_working/ragon/CAVD2015Jul/"
+path <- "/shared/silo_researcher/Gottardo_R/pvan_working/improvedMindensity/"
+setwd(path)
+
+# load the data
+gs <- load_gs(paste(dataPath, "output/gs_auto/", sep="/"))
+pd <- pData(gs)
+
+# plot the markers
+#  png("output/improvedMindensity_live_marker_Sep17.png", width=1000, height=1000)
+#  plotGate(gs, "live", type="densityplot", main="live(Rh103Di) marker, improvedMindensity Sep17" )
+#  dev.off()
+#  
+# png("output/improvedMindensity_CD19_marker_Sep17.png", width=1000, height=1000)
+# plotGate(gs, "live/CD19neg", type="densityplot", main="CD19(Nd142Di) marker, improvedMindensity Sep17")
+# dev.off()
+
+# Rh103Di = live
+# Nd142Di = CD19
+# Sm154Di = CD3
+# Nd145Di = CD4
+# Nd146Di = CD8a
+# Er167Di = CD27
+# Nd144Di = CD38
+
+fs <- getData(gs, "live")
+markers <- pData(parameters(fs[[1]]))
+
+chnls <- markers[!is.na(markers$desc),]
+chnls <- chnls[chnls$desc != "EQ_Beads",]
+chnls <- chnls[!grepl("DNA", chnls$desc),]
+chnls <- chnls[!grepl("IL", chnls$desc),]
+chnls <- chnls[!grepl("Granzyme", chnls$desc),]
+chnls <- chnls[!grepl("PD_1", chnls$desc),]
+chnls <- chnls[!grepl("IFN_g", chnls$desc),]
+chnls <- chnls[!grepl("TNF_a", chnls$desc),]
+chnls <- chnls[!grepl("CD107a", chnls$desc),]
+chnls <- chnls[!grepl("Granzyme", chnls$desc),]
+chnls <- chnls[!grepl("Perforin", chnls$desc),]
+chnls <- as.vector(chnls$name)
+names(chnls) <- NULL
+
+#chnls <- c("Rh103Di", "Nd142Di", "Sm154Di")
+
+source("improvedMindensity.R")
+
+gates <- getChannelsPops(gs)
+gates <- gates[2]
+
+out <- getSampleStats(gs, gates)
+sampleStats <- out[[1]]
+featureList <- out[[2]]
+
+chnls <- unlist(gates)
+
+sampleStats <- flagBadSamples(sampleStats, chnl)
+sampleStats[channel == "Rh103Di" & flags != ""]
+
+# Rh103Di = live
+# Nd142Di = CD19
+# Sm154Di = CD3
+# Nd145Di = CD4
+# Nd146Di = CD8a
+# Er167Di = CD27
+# Nd144Di = CD38
+
+# save stats
+save(sampleStats, file="output/sampleStats.RData")
+save(featureList, file="output/featureList.RData")
+
+# regate problematic samples
+regateBadSamples(gs, sampleStats, gates, plot=F, verbose=T)
+
